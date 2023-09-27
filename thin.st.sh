b75eb5374a1e1f3d0a8d1b6766f5ffa1af81ba2d
MSL6030 <82145523+MSL6030@users.noreply.github.com>
2023-09-27 16:09:36 +0300

    Update thin.st.sh

--- thin.st.sh
+++ thin.st.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 vmware-view --serverURL=x315.krost.net --fullscreen --singleAutoConnect --nomenubar --once
-PS3="Введите номер: "
-select m in Подключиться_заново Тестировать_сеть Выключить_компьютер
+shut=0
+while shut==0
 do
-	if [$m=="Выключить_компьютер"]
-	then
-		echo OK
-		break
-	fi
-	vmware-view бла бла
+	clear
+	echo Меню:
+	echo 1 - Подключиться заново
+	echo 2 - Тестировать сеть
+	echo 3 - Выключить компьютер
+	read -p 'Введите номер: ' result
+	case $result in
+ 		1)
+  			vmware-view --serverURL=x315.krost.net --fullscreen --singleAutoConnect --nomenubar --once
+    		;;
+     		2)
+      			ping ya.ru -c 5
+			echo test
+  			echo test
+    		;;
+     		3)
+      			shut=1
+	 		echo Пока!
+		;;
+ 		*)
+  			echo Нет такого меню
+    			sleep 3
+      		;;
+	esac
 done
 shutdown now
